4c90a2dd1ac6b6ad66250a6d350fbf4cf8d9727e
Jacob Thompson <jacob.thompson@wiwasolvet.ca>
2022-05-09 14:57:05 -0300

    Dev has a more complete version of DasDds.sh

--- DasDds.sh
+++ DasDds.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+export $(xargs < .env)
+if [ -z $DATASETS_DIR ]; then
+  DATASETS_DIR=/srv/data/erddap
+fi
 docker run --rm -it \
-  -v "$(pwd)/datasets:/datasets" \
+  -v "${DATASETS_DIR}:/datasets" \
   -v "$(pwd)/logs:/erddapData/logs" \
   -v "$(pwd)/erddap/content:/usr/local/tomcat/content/erddap" \
-  axiom/docker-erddap:latest \
+  ${IMAGE} \
   bash -c "cd webapps/erddap/WEB-INF/ && bash DasDds.sh"
-  
+

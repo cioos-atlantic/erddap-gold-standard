87dd265e41d64c1480b2612f1a48fffc327a6675
Jessy Barrette <jessy.barrette@hakai.org>
2022-05-16 12:11:00 -0400

    Split dataset.xml into datasets/{datasetID}.xml and add concat script

---
+++ ConcatDatasetXml.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DATASET_XML_DIR="./config/datasets_xml/"
+DATASET_XML_FILE="./config/datasets.xml"
+ENCODING="UTF-8"
+​
+echo '<?xml version="1.0" encoding="UTF-8" ?>' > ${DATASET_XML_FILE}
+echo "<erddapDatasets>" >> ${DATASET_XML_FILE}
+cat ${DATASET_XML_DIR}*.xml >> ${DATASET_XML_FILE}
+echo "</erddapDatasets>" >> ${DATASET_XML_FILE}
